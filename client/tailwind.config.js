2c014a2a8fc580e397e1b87e72f50df7e41b9598
Omar Asim Mirza <omar8786270@gmail.com>
2024-12-02 20:29:36 +0300

    reverted changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -20,18 +20,15 @@ module.exports = {
 
       xxs: { max: "440px" },
 
-      // th: { raw: "(max-width: 350px)  " }, iphone13
+      th: { raw: "(max-width: 350px)  " },
 
-      // se: { raw: "(max-width: 380px) and (min-width: 350px)" }, iphoneSE
+      se: { raw: "(max-width: 380px) and (min-width: 350px)" },
 
-      // gal: { raw: "(width: 360px) and (height: 740px)"}, galaxy
+      gal: { raw: "(width: 360px) and (height: 740px)"},
 
-      // nest: { raw: "(width: 1024px) and (height: 600px)" }, 
+      nest: { raw: "(width: 1024px) and (height: 600px)" }, 
 
-      // final: {raw: "(width: 1024px) and (height: 600px)"}
-      se: { max: "575px" }, // Mobile (iPhone 3 - iPhone XS Max).
-      th: { min: "576px", max: "897px" }, // Mobile (matches max: iPhone 11 Pro Max landscape @ 896px).
-      nest: { min: "898px", max: "1199px" }, // Tablet (matches max: iPad Pro @ 1112px).
+      final: {raw: "(width: 1024px) and (height: 600px)"}
     },
   },
   plugins: [],
