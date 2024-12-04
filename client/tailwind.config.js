80004ef98a4925de0cd87382a1bf3e7f44af1313
Omar Asim Mirza <omar8786270@gmail.com>
2024-12-04 18:06:07 +0300

    testing new styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,23 +12,10 @@ module.exports = {
       },
     },
     screens: {
-      lg: { max: "2023px" },
-
-      sm: { max: "1000px" },
-
-      xs: { max: "670px" },
-
-      xxs: { max: "440px" },
-
-      th: { raw: "(max-width: 350px) and (orientation: portrait)" },
-
-      se: { raw: "(max-width: 380px) and (min-width: 350px) and (orientation: portrait)" },
-
-      gal: { raw: "(width: 360px) and (height: 740px) and (orientation: portrait)"},
-
-      nest: { raw: "(width: 1024px) and (height: 600px) and (orientation: portrait)" }, 
-
-      final: {raw: "(width: 1024px) and (height: 600px) and (orientation: portrait)"}
+      lg: { max: "1280px" }, 
+      md: { max: "1024px" }, 
+      sm: { max: "768px" }, 
+      xs: { max: "480px" },
     },
   },
   plugins: [],
